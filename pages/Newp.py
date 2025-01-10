a51bce9767153ff4f488e54bc88de0f58755a141
jeenykim <110590234+jeenykim@users.noreply.github.com>
2025-01-10 16:47:13 +0900

    Your commit message

---
+++ pages/Newp.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+from urllib.error import URLError
+
+# 페이지 기본 설정
+st.set_page_config(
+    page_icon="🤩🐙😍",
+    page_title="지니킴의 스트림릿 배포하기",
+    layout="wide",
+)
+# VSCode 에서 이모지(emoji)
+# Windows 환경 
+# 'Win' + '.'
+# Mac 환경
+# 'control' + 'command' + 'Space'
+
+st.markdown("# Mapping Demo")
+st.sidebar.header("Mapping Demo")
+st.write(
+    """This demo shows how to use
+[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
+to display geospatial data."""
+)
+
+
+# 수정된 부분: @st.experimental_memo 대신 @st.cache_data 사용
+@st.cache_data
+def from_data_file(filename):
+    url = (
+        "http://raw.githubusercontent.com/streamlit/"
+        "example-data/master/hello/v1/%s" % filename
+    )
+    return pd.read_json(url)
+
+
+try:
+    ALL_LAYERS = {
+        "Bike Rentals": pdk.Layer(
+            "HexagonLayer",
+            data=from_data_file("bike_rental_stats.json"),
+            get_position=["lon", "lat"],
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            extruded=True,
+        ),
+        "Bart Stop Exits": pdk.Layer(
+            "ScatterplotLayer",
+            data=from_data_file("bart_stop_stats.json"),
+            get_position=["lon", "lat"],
+            get_color=[200, 30, 0, 160],
+            get_radius="[exits]",
+            radius_scale=0.05,
+        ),
+    }
+    st.sidebar.markdown("### Map Layers")
+    selected_layers = [
+        layer
+        for layer_name, layer in ALL_LAYERS.items()
+        if st.sidebar.checkbox(layer_name, True)
+    ]
+    if selected_layers:
+        st.pydeck_chart(
+            pdk.Deck(
+                map_style="mapbox://styles/mapbox/light-v9",
+                initial_view_state={
+                    "latitude": 37.76,
+                    "longitude": -122.4,
+                    "zoom": 11,
+                    "pitch": 50,
+                },
+                layers=selected_layers,
+            )
+        )
+    else:
+        st.error("Please choose at least one layer above.")
+except URLError as e:
+    st.error(
+        """
+        **This demo requires internet access.**
+        Connection error: %s
+    """
+        % e.reason
+    )
